fb1dd753e23eaf4b7bac172dbcceafdc69c27c81
Julia Schlumpp <julia@unikraft.io>
2025-06-02 10:45:12 +0200

    Remove smear-cursor

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -87,18 +87,6 @@ return {
         opts = {},
     },
     {
-        "sphamba/smear-cursor.nvim",
-        event = "VeryLazy",
-        opts = {
-            stiffness = 0.8,
-            trailing_stiffness = 0.5,
-            stiffness_insert_mode = 0.6,
-            trailing_stiffness_insert_mode = 0.6,
-            distance_stop_animating = 0.5,
-            smear_insert_mode = false,
-        },
-    },
-    {
         'anuvyklack/windows.nvim',
         dependencies = {
             'anuvyklack/middleclass',
