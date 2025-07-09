71a5bb954563b70583d33e06e24a4b5e01c4e40e
Julia Schlumpp <julia@unikraft.io>
2025-07-09 14:16:24 +0200

    Adjust colorcolumn
    Signed-off-by: Julia Schlumpp <julia@unikraft.io>

--- init.lua
+++ init.lua
@@ -40,6 +40,7 @@ option('inccommand', 'split')
 option('list', true, window)
 option('mousemodel', 'extend')
 option('listchars', 'lead:·,tab:> ,nbsp:+,trail:-')
+option('colorcolumn', '+1', window)
 vim.opt.formatoptions:remove { 't' }
 vim.opt.formatoptions:append { '/' }
 
