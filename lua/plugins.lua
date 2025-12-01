f05d80ec297c6141e0b4bff1851feba53b7dc8c8
Julia Schlumpp <julia@unikraft.io>
2025-12-01 09:56:35 +0100

    Replace vim-orgmode with orgmode
    Signed-off-by: Julia Schlumpp <julia@unikraft.io>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -181,8 +181,15 @@ return {
         ft = 'zig',
     },
     {
-        'jceb/vim-orgmode',
-        ft = 'org',
+        'nvim-orgmode/orgmode',
+        event = 'VeryLazy',
+        ft = { 'org' },
+        config = function()
+            require('orgmode').setup({
+                org_agenda_files = '~/org/**/*',
+                org_default_notes_file = '~/org/refile.org',
+            })
+        end,
     },
     {
         'bakpakin/fennel.vim',
