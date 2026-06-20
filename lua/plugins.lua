f6ba39c54e595628e4048d6d24541f9f4301c242
Julia Schlumpp <julia@schlumpp.com>
2026-06-21 00:34:18 +0200

    Disable autopair for lisp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -630,7 +630,11 @@ return {
         event = { 'InsertEnter' },
         opts = {
             fast_wrap = {},
-            disable_filetype = { 'uiua' },
+            disable_filetype = {
+                'uiua',
+                'clojure',
+                'fennel',
+            },
             check_ts = true,
         },
         config = function(plugin, opts)
